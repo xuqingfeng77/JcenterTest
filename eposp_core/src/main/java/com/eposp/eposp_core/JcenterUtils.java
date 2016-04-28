33e757fd91a70d5e73f7a76d7cd8bf5afa628c5a
xuqingfeng77 <xuqingfeng77@foxmail.com>
2016-04-28 17:35:52 +0800

    udpate

---
+++ eposp_core/src/main/java/com/eposp/eposp_core/JcenterUtils.java
@@ -0,0 +1,14 @@
+package com.eposp.eposp_core;
+
+/**
+ * Created by xuqingfeng on 16/4/28.
+ */
+public class JcenterUtils {
+    public int getMath(){
+        return 1;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
